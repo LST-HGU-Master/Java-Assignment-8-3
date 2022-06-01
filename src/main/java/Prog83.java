50b2496ccd3fd7e80f4f5f07028371acc4aa1d87
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-01 21:15:49 +0900

    Update Prog83.java

--- src/main/java/Prog83.java
+++ src/main/java/Prog83.java
@@ -1,9 +1,16 @@
 public class Prog83 {
 
-	public static void main(String[] args) {
-		Matango m1 = new Matango();
-		Matango m2 = new Matango();
-	}
+    public static void main(String[] args) {
+        Matango m1 = new Matango();
+        System.out.println("お化けキノコ" + m1.suffix + "が誕生しました！");
+        Matango m2 = new Matango();
+        System.out.println("お化けキノコ" + m2.suffix + "が誕生しました！");            
+    
+        Matango [] mts = new Matango[27];
+        for( Matango mt: mts){
+            mt = new Matango();
+            System.out.println( "お化けキノコ" + mt.suffix + "が誕生しました！");
+        }
+    }
 
 }
-
